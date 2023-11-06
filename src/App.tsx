3ab80b5543edab66ea642755eb8dbde3b3840f9a
David Martinelli <david.martinelli@farfetch.com>
2023-11-06 17:53:55 +0000

    migrate to player app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Provider as ReduxProvider } from 'react-redux';
-import { AddTodo } from './components/AddTodo';
-import { TodoList } from './components/TodoList';
 import { store } from './store';
+import { Player } from './pages/Player';
+import './styles/global.css';
 
 export function App() {
 	return (
 		<ReduxProvider store={store}>
-			<div className="">
-				<AddTodo />
-				<TodoList />
+			<div className="bg-red-600">
+				<Player />
 			</div>
 		</ReduxProvider>
 	);
